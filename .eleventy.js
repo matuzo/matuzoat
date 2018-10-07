20a99a83abb359f86bf0824e0e9b75fd461463e8
Manuel Matuzovic <m.matuzovic@nousdigital.net>
2018-10-07 10:33:58 +0200

    Move identity widget to head

---
+++ .eleventy.js
@@ -0,0 +1,30 @@
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addPassthroughCopy("static/img/uploads");
+  eleventyConfig.addPassthroughCopy("admin");
+
+  return {
+    templateFormats: [
+      "md",
+      "njk",
+      "html"
+    ],
+
+    // If your site lives in a different subdirectory, change this.
+    // Leading or trailing slashes are all normalized away, so don’t worry about it.
+    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+    // This is only used for URLs (it does not affect your file structure)
+    pathPrefix: "/",
+
+    markdownTemplateEngine: "liquid",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    passthroughFileCopy: true,
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
+  };
+};
