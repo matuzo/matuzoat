eb99b0e57b032c4515bad1d1986948f7b2b05fe0
Manuel Matuzovic <manuel@matuzo.at>
2019-01-04 10:52:54 +0100

    Fix build task

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const common = require('./webpack.common.js');
 const merge = require('webpack-merge');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = merge(common, {
   optimization: {
