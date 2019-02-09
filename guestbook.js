49a5e0cf9e60cdc3926e9daab22c8a8d6f3c3094
Manuel Matuzovic <manuel@matuzo.at>
2019-02-09 17:05:28 +0100

    Display submissions in guestbook

---
+++ guestbook.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+const fs = require('fs');
+
+const form_id = '5c5e9b789ae4f40008e8c761';
+const access_token = process.env.NETLIFY_TOKEN;
+
+const guestbook = {
+  submissions: []
+}
+
+// Make a request for a user with a given ID
+axios.get(`https://api.netlify.com/api/v1/forms/${form_id}/submissions/?access_token=${access_token}`)
+  .then(function (response) {
+    // handle success
+    guestbook.submissions = response.data;
+
+    fs.writeFileSync('./src/pages/guestbook/guestbook.json', JSON.stringify(guestbook));
+    
+    console.log("*** File written successfully");
+
+
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
