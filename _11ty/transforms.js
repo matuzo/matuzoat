1cdc7bf7db3e920a01cc1487d1958e8c079739f5
Manuel Matuzovic <m.matuzovic@nousdigital.net>
2019-05-14 08:18:06 +0200

    Add anchors to h3s

--- _11ty/transforms.js
+++ _11ty/transforms.js
@@ -13,11 +13,21 @@ module.exports = {
 
   anchors: (content, outputPath) => {
     if( outputPath.includes('/blog/') ) {
-      return content.replaceAll(new RegExp("<h2>(.*)</h2>","i"), function(match, heading) {
+      var content_anchors;
+
+      // TODO: Clean this mess up
+      content_anchors = content.replaceAll(new RegExp("<h2>(.*)</h2>","i"), function(match, heading) {
         var slug = slugify(heading, { lower: true, remove: /[*+~.()'"!?:@]/g });
         headings.push([slug, heading]);
       
         return `<h2 id="${slug}">${heading}</h2>`;
+      });
+
+      return content_anchors.replaceAll(new RegExp("<h3>(.*)</h3>","i"), function(match, heading) {
+        var slug = slugify(heading, { lower: true, remove: /[*+~.()'"!?:@]/g });
+        headings.push([slug, heading]);
+      
+        return `<h3 id="${slug}">${heading}</h3>`;
       });
     }
     return content;
