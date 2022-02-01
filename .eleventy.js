7c932dc4458019148bafa7fb718c74022608bfc9
Manuel Matuzovic <manuel@matuzo.at>
2022-02-01 21:28:46 +0100

    Move layouts into folder

--- .eleventy.js
+++ .eleventy.js
@@ -1,58 +1,29 @@
 const filters = require('./_11ty/filters.js');
-const collections = require('./_11ty/collections.js');
-const transforms = require('./_11ty/transforms.js');
 
 const pluginRss = require('@11ty/eleventy-plugin-rss');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
-var escape = require('escape-html');
 
 module.exports = function (eleventyConfig) {
+  // Enable Quiet Mode to Reduce Console Noise 
+  eleventyConfig.setQuietMode(true);
+
   // Filters
   Object.keys(filters).forEach((filterName) => {
     eleventyConfig.addFilter(filterName, filters[filterName]);
   });
 
-  // Collections
-  Object.keys(collections).forEach((collectionName) => {
-    eleventyConfig.addCollection(collectionName, collections[collectionName]);
-  });
-
-  // Transforms
-  Object.keys(transforms).forEach((transformName) => {
-    eleventyConfig.addTransform(transformName, transforms[transformName]);
-  });
-
-  eleventyConfig.addPairedShortcode('escape', function (content) {
-    return escape(content);
-  });
-
   // Plugins
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(syntaxHighlight);
-
-  eleventyConfig.addPassthroughCopy('./src/static/img');
-  eleventyConfig.addPassthroughCopy('./src/static/min');
-  eleventyConfig.addPassthroughCopy('./src/images');
-  eleventyConfig.addPassthroughCopy('admin');
-  eleventyConfig.addPassthroughCopy('_redirects');
-  eleventyConfig.addPassthroughCopy({
-    './src/static/favicon': '/',
-    './src/static/css': '/assets',
-  });
-
+  
   return {
-    templateFormats: ['md', 'njk'],
-
-    pathPrefix: '/',
-
-    markdownTemplateEngine: 'liquid',
+    markdownTemplateEngine: 'njk',
     htmlTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
     passthroughFileCopy: true,
     dir: {
       input: 'src',
-      includes: '_includes',
-      data: '_data',
+      layouts: '_layouts',
       output: '_site',
     },
   };
